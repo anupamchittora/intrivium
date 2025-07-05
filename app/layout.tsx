102803ea4c68ad1602b4c3263b1aba75901a7369
anupamchittora <2022uec1452@mnit.ac.in>
2025-07-05 21:02:40 +0530

    setup app, file and folder structure, routing and auth pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,31 @@
 import type { Metadata } from "next";
-import { Mona_Sans} from "next/font/google";
+import { Mona_Sans } from "next/font/google";
+
 import "./globals.css";
+import {Toaster} from "sonner";
 
 const monaSans = Mona_Sans({
-  variable: "--font-mona-sans",
-  subsets: ["latin"],
+    variable: "--font-mona-sans",
+    subsets: ["latin"],
 });
 
-
 export const metadata: Metadata = {
-  title: "Intrivium",
-  description: "An AI powered platform for preparing for Mock Interviews ",
+    title: "PrepWise",
+    description: "An AI-powered platform for preparing for mock interviews",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="dark">
-      <body
-        className={` ${monaSans.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="dark">
+            <body className={`${monaSans.className} antialiased pattern`}>
+                {children}
+
+                <Toaster />
+            </body>
+        </html>
+    );
 }
