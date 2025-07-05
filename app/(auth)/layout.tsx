a3f9ff13ea833670d4fd4c5797c747b73bd5141f
anupamchittora <2022uec1452@mnit.ac.in>
2025-07-05 21:03:10 +0530

    setup app, file and folder structure, routing and auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
-import { ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 
-const AuthLayout = ({ children }: { children: ReactNode  }) => {
-    return (
-        <div className="auth-layout">{children}</div>
-    )
+const AuthLayout = ({children}: {children: ReactNode}) => {
+  return (
+    <div>{children}</div>
+  )
 }
 
 export default AuthLayout
