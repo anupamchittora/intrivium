d73f2c62d09948153191bd07f1412cd59a3127f5
anupamchittora <2022uec1452@mnit.ac.in>
2025-07-10 17:44:41 +0530

    Interview card generated on homepage

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCgrCXECTzQlJzvvqyETadxNEEqHZfnSiE",
-  authDomain: "intrivium.firebaseapp.com",
-  projectId: "intrivium",
-  storageBucket: "intrivium.firebasestorage.app",
-  messagingSenderId: "626347636074",
-  appId: "1:626347636074:web:e4e0394a66bb53537de113",
-  measurementId: "G-69ZS7FMDBB"
+  apiKey: "AIzaSyC56WMQtXafCHG0zmRp4-skMIqcY_zgCc0",
+  authDomain: "prepwise-58989.firebaseapp.com",
+  projectId: "prepwise-58989",
+  storageBucket: "prepwise-58989.firebasestorage.app",
+  messagingSenderId: "996187243661",
+  appId: "1:996187243661:web:e3e13596cac227bdb8cec4",
+  measurementId: "G-MSYR77XMNF"
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
